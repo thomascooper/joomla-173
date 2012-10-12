8932563a517e156fba53f37143f4d39288830fb3
root <root@sql1.worldwideinterweb.com>
2012-10-11 23:00:49 -0500

    first push from production

---
+++ logs/error.php
@@ -0,0 +1,153 @@
+#<?php die('Forbidden.'); ?>
+#Date: 2011-11-28 18:53:03 UTC
+#Software: Joomla Platform 11.2.0 Stable+Modified [ Omar ] 27-Jul-2011 00:00 GMT
+
+#Fields: date	time	priority	clientip	category	message
+2011-11-28	18:53:03	INFO	74.212.251.170	Joomla FAILURE: 	Invalid password
+2011-11-28	18:53:13	INFO	74.212.251.170	Joomla FAILURE: 	Invalid password
+2011-12-09	17:52:22	INFO	74.212.251.170	Joomla FAILURE: 	Invalid password
+2011-12-16	01:19:39	INFO	74.212.251.170	Joomla FAILURE: 	Invalid password
+2011-12-16	23:47:37	INFO	74.212.251.170	Joomla FAILURE: 	User does not exist
+2012-01-03	04:27:52	INFO	75.84.221.16	Joomla FAILURE: 	Invalid password
+2012-01-03	04:28:07	INFO	75.84.221.16	Joomla FAILURE: 	Invalid password
+2012-01-05	19:43:51	INFO	206.183.2.183	Joomla FAILURE: 	Invalid password
+2012-01-05	19:43:59	INFO	206.183.2.183	Joomla FAILURE: 	Invalid password
+2012-01-05	19:46:27	INFO	206.183.2.183	Joomla FAILURE: 	Invalid password
+2012-01-28	02:26:03	INFO	74.212.251.170	Joomla FAILURE: 	Invalid password
+2012-01-28	03:49:24	INFO	74.212.251.170	Joomla FAILURE: 	Invalid password
+2012-01-30	20:08:23	INFO	74.212.251.170	Joomla FAILURE: 	Invalid password
+2012-01-31	02:54:09	INFO	74.212.251.170	Joomla FAILURE: 	Invalid password
+2012-01-31	15:02:23	INFO	75.84.221.16	Joomla FAILURE: 	Invalid password
+2012-02-01	22:03:12	INFO	74.212.251.170	Joomla FAILURE: 	Invalid password
+2012-02-03	21:43:38	INFO	206.183.2.183	Joomla FAILURE: 	Invalid password
+2012-02-04	02:13:27	INFO	74.212.251.170	Joomla FAILURE: 	Invalid password
+2012-02-07	22:06:52	INFO	74.212.251.170	Joomla FAILURE: 	Invalid password
+2012-02-14	18:25:32	INFO	74.212.251.170	Joomla FAILURE: 	Invalid password
+2012-02-18	02:25:03	INFO	74.212.251.170	Joomla FAILURE: 	Invalid password
+2012-02-22	15:38:59	INFO	76.91.142.183	Joomla FAILURE: 	Empty password not allowed
+2012-02-23	15:06:37	INFO	75.84.221.16	Joomla FAILURE: 	Empty password not allowed
+2012-02-23	15:06:56	INFO	75.84.221.16	Joomla FAILURE: 	Invalid password
+2012-02-24	18:35:51	INFO	74.212.251.170	Joomla FAILURE: 	Invalid password
+2012-02-29	23:07:34	INFO	74.212.251.170	Joomla FAILURE: 	Invalid password
+2012-03-02	18:36:48	INFO	74.212.251.170	Joomla FAILURE: 	Invalid password
+2012-03-07	18:40:47	INFO	74.212.251.170	Joomla FAILURE: 	Invalid password
+2012-03-08	02:54:14	INFO	74.212.251.170	Joomla FAILURE: 	Invalid password
+2012-03-10	02:29:33	INFO	74.212.251.170	Joomla FAILURE: 	Invalid password
+2012-03-10	02:29:40	INFO	74.212.251.170	Joomla FAILURE: 	Invalid password
+2012-03-10	02:29:49	INFO	74.212.251.170	Joomla FAILURE: 	Invalid password
+2012-03-12	04:48:35	INFO	75.84.221.16	Joomla FAILURE: 	Invalid password
+2012-03-12	04:48:45	INFO	75.84.221.16	Joomla FAILURE: 	Invalid password
+2012-03-12	04:48:58	INFO	75.84.221.16	Joomla FAILURE: 	Empty password not allowed
+2012-03-12	04:49:09	INFO	75.84.221.16	Joomla FAILURE: 	Invalid password
+2012-03-14	07:30:11	INFO	75.84.221.16	Joomla FAILURE: 	Invalid password
+2012-03-18	00:06:22	INFO	75.84.221.16	Joomla FAILURE: 	Invalid password
+2012-03-18	05:29:59	INFO	174.100.47.243	Joomla FAILURE: 	Invalid password
+2012-03-18	05:30:18	INFO	174.100.47.243	Joomla FAILURE: 	Invalid password
+2012-03-20	20:24:22	INFO	74.212.251.170	Joomla FAILURE: 	Invalid password
+2012-03-20	20:24:32	INFO	74.212.251.170	Joomla FAILURE: 	Invalid password
+2012-03-21	04:29:42	INFO	75.84.221.16	Joomla FAILURE: 	Invalid password
+2012-03-22	00:58:04	INFO	74.212.251.170	Joomla FAILURE: 	Invalid password
+2012-03-22	03:11:45	INFO	75.84.221.16	Joomla FAILURE: 	Invalid password
+2012-03-22	07:32:33	INFO	75.84.221.16	Joomla FAILURE: 	Invalid password
+2012-03-26	17:25:17	INFO	74.212.251.170	Joomla FAILURE: 	Invalid password
+2012-03-27	19:14:28	INFO	74.212.251.170	Joomla FAILURE: 	Invalid password
+2012-03-27	19:14:36	INFO	74.212.251.170	Joomla FAILURE: 	Invalid password
+2012-03-28	02:35:11	INFO	74.212.251.170	Joomla FAILURE: 	Invalid password
+2012-03-30	05:31:57	INFO	76.175.156.214	Joomla FAILURE: 	Invalid password
+2012-03-30	05:32:05	INFO	76.175.156.214	Joomla FAILURE: 	Invalid password
+2012-03-30	21:31:10	INFO	74.212.251.170	Joomla FAILURE: 	Invalid password
+2012-04-08	20:50:04	INFO	76.175.156.214	Joomla FAILURE: 	Invalid password
+2012-04-09	20:21:41	INFO	74.212.251.170	Joomla FAILURE: 	Invalid password
+2012-04-09	20:21:49	INFO	74.212.251.170	Joomla FAILURE: 	Invalid password
+2012-04-16	05:26:38	INFO	76.175.156.214	Joomla FAILURE: 	Invalid password
+2012-04-20	20:47:30	INFO	74.212.251.170	Joomla FAILURE: 	Invalid password
+2012-04-26	01:27:59	INFO	74.212.251.170	Joomla FAILURE: 	Invalid password
+2012-04-27	04:39:43	INFO	76.175.156.214	Joomla FAILURE: 	Invalid password
+2012-04-30	01:43:26	INFO	76.175.156.214	Joomla FAILURE: 	Invalid password
+2012-04-30	01:43:39	INFO	76.175.156.214	Joomla FAILURE: 	Invalid password
+2012-04-30	01:43:48	INFO	76.175.156.214	Joomla FAILURE: 	Invalid password
+2012-05-01	17:39:17	INFO	74.212.251.170	Joomla FAILURE: 	Invalid password
+2012-05-01	20:35:23	INFO	74.212.251.170	Joomla FAILURE: 	Invalid password
+2012-05-03	17:58:56	INFO	74.212.251.170	Joomla FAILURE: 	Empty password not allowed
+2012-05-08	19:28:35	INFO	74.212.251.170	Joomla FAILURE: 	Invalid password
+2012-05-09	02:24:50	INFO	74.212.251.170	Joomla FAILURE: 	Invalid password
+2012-05-10	18:02:56	INFO	74.212.251.170	Joomla FAILURE: 	Empty password not allowed
+2012-05-11	21:15:30	INFO	74.212.251.170	Joomla FAILURE: 	Invalid password
+2012-05-14	12:24:10	INFO	76.175.156.214	Joomla FAILURE: 	Empty password not allowed
+2012-05-14	12:24:30	INFO	76.175.156.214	Joomla FAILURE: 	Empty password not allowed
+2012-05-14	12:24:51	INFO	76.175.156.214	Joomla FAILURE: 	Invalid password
+2012-05-14	12:25:12	INFO	76.175.156.214	Joomla FAILURE: 	Invalid password
+2012-05-14	12:25:32	INFO	76.175.156.214	Joomla FAILURE: 	Invalid password
+2012-05-14	12:26:18	INFO	76.175.156.214	Joomla FAILURE: 	Invalid password
+2012-05-17	16:55:16	INFO	74.212.251.170	Joomla FAILURE: 	Invalid password
+2012-05-18	06:56:17	INFO	76.175.156.214	Joomla FAILURE: 	Invalid password
+2012-05-18	06:56:27	INFO	76.175.156.214	Joomla FAILURE: 	Invalid password
+2012-05-22	23:04:32	INFO	74.212.251.170	Joomla FAILURE: 	Invalid password
+2012-05-28	20:59:49	INFO	76.175.156.214	Joomla FAILURE: 	Invalid password
+2012-05-31	17:07:40	INFO	74.212.251.170	Joomla FAILURE: 	Invalid password
+2012-05-31	18:04:13	INFO	74.212.251.170	Joomla FAILURE: 	Invalid password
+2012-05-31	23:58:53	INFO	74.212.251.170	Joomla FAILURE: 	Invalid password
+2012-06-07	17:42:04	INFO	74.212.251.170	Joomla FAILURE: 	Invalid password
+2012-06-11	02:53:10	INFO	76.175.156.214	Joomla FAILURE: 	Invalid password
+2012-06-12	05:42:02	INFO	76.175.156.214	Joomla FAILURE: 	Invalid password
+2012-06-13	06:28:08	INFO	76.175.156.214	Joomla FAILURE: 	Invalid password
+2012-06-13	06:28:15	INFO	76.175.156.214	Joomla FAILURE: 	Invalid password
+2012-06-13	06:28:23	INFO	76.175.156.214	Joomla FAILURE: 	Invalid password
+2012-06-14	21:09:50	INFO	74.212.251.170	Joomla FAILURE: 	Invalid password
+2012-06-15	02:49:21	INFO	74.212.251.170	Joomla FAILURE: 	Invalid password
+2012-06-29	09:03:06	INFO	76.175.156.214	Joomla FAILURE: 	Invalid password
+2012-06-30	06:16:12	INFO	76.175.156.214	Joomla FAILURE: 	Invalid password
+2012-06-30	06:16:21	INFO	76.175.156.214	Joomla FAILURE: 	Invalid password
+2012-07-10	00:22:22	INFO	74.212.251.170	Joomla FAILURE: 	Invalid password
+2012-07-10	00:22:34	INFO	74.212.251.170	Joomla FAILURE: 	Invalid password
+2012-07-14	00:58:12	INFO	76.175.156.214	Joomla FAILURE: 	Invalid password
+2012-07-17	00:02:09	INFO	74.212.251.170	Joomla FAILURE: 	Invalid password
+2012-07-17	00:02:17	INFO	74.212.251.170	Joomla FAILURE: 	Invalid password
+2012-07-17	01:40:49	INFO	74.212.251.170	Joomla FAILURE: 	Invalid password
+2012-07-17	03:02:39	INFO	174.100.47.243	Joomla FAILURE: 	Invalid password
+2012-07-17	03:03:06	INFO	174.100.47.243	Joomla FAILURE: 	Invalid password
+2012-07-17	03:03:18	INFO	174.100.47.243	Joomla FAILURE: 	Invalid password
+2012-07-17	03:03:24	INFO	174.100.47.243	Joomla FAILURE: 	Invalid password
+2012-07-17	03:10:21	INFO	174.100.47.243	Joomla FAILURE: 	User does not exist
+2012-07-17	03:10:28	INFO	174.100.47.243	Joomla FAILURE: 	User does not exist
+2012-07-17	03:10:34	INFO	174.100.47.243	Joomla FAILURE: 	User does not exist
+2012-07-17	03:10:47	INFO	174.100.47.243	Joomla FAILURE: 	User does not exist
+2012-07-17	03:11:23	INFO	174.100.47.243	Joomla FAILURE: 	User does not exist
+2012-07-17	03:11:29	INFO	174.100.47.243	Joomla FAILURE: 	User does not exist
+2012-07-17	03:42:48	INFO	174.100.47.243	Joomla FAILURE: 	User does not exist
+2012-07-17	23:10:17	INFO	74.212.251.170	Joomla FAILURE: 	Invalid password
+2012-07-17	23:10:24	INFO	74.212.251.170	Joomla FAILURE: 	Invalid password
+2012-07-17	23:10:32	INFO	74.212.251.170	Joomla FAILURE: 	Invalid password
+2012-07-20	22:35:46	INFO	74.212.251.170	Joomla FAILURE: 	Invalid password
+2012-07-27	18:19:26	INFO	66.87.127.254	Joomla FAILURE: 	Invalid password
+2012-07-27	18:20:18	INFO	66.87.127.254	Joomla FAILURE: 	Invalid password
+2012-08-01	08:33:24	INFO	76.175.156.214	Joomla FAILURE: 	Invalid password
+2012-08-01	08:33:36	INFO	76.175.156.214	Joomla FAILURE: 	Invalid password
+2012-08-05	01:16:42	INFO	76.175.156.214	Joomla FAILURE: 	Invalid password
+2012-08-05	19:56:28	INFO	76.175.156.214	Joomla FAILURE: 	Invalid password
+2012-08-06	18:14:11	INFO	74.212.251.170	Joomla FAILURE: 	Invalid password
+2012-08-21	00:57:48	INFO	74.212.251.170	Joomla FAILURE: 	Invalid password
+2012-08-22	06:39:34	INFO	76.175.156.214	Joomla FAILURE: 	Invalid password
+2012-08-23	19:20:22	INFO	74.212.251.170	Joomla FAILURE: 	Invalid password
+2012-08-25	00:25:03	INFO	74.212.251.170	Joomla FAILURE: 	Invalid password
+2012-08-27	01:49:47	INFO	76.175.156.214	Joomla FAILURE: 	Invalid password
+2012-08-30	08:24:23	INFO	76.175.156.214	Joomla FAILURE: 	Invalid password
+2012-09-07	14:41:07	INFO	98.103.25.72	Joomla FAILURE: 	User does not exist
+2012-09-07	14:41:15	INFO	98.103.25.72	Joomla FAILURE: 	Invalid password
+2012-09-21	23:20:06	INFO	74.212.251.170	Joomla FAILURE: 	Invalid password
+2012-09-23	12:36:13	INFO	46.160.85.231	Joomla FAILURE: 	Invalid password
+2012-09-23	22:53:58	INFO	76.175.156.214	Joomla FAILURE: 	Invalid password
+2012-09-24	03:47:21	INFO	174.100.47.243	Joomla FAILURE: 	Invalid password
+2012-09-24	14:24:40	INFO	46.160.85.231	Joomla FAILURE: 	Invalid password
+2012-09-26	21:52:21	INFO	74.212.251.170	Joomla FAILURE: 	Invalid password
+2012-09-27	21:21:56	INFO	74.212.251.170	Joomla FAILURE: 	Invalid password
+2012-09-28	21:03:06	INFO	74.212.251.170	Joomla FAILURE: 	Invalid password
+2012-09-29	10:18:54	INFO	46.160.85.231	Joomla FAILURE: 	Invalid password
+2012-10-01	13:07:19	INFO	46.160.85.231	Joomla FAILURE: 	Invalid password
+2012-10-02	05:44:15	INFO	46.160.85.231	Joomla FAILURE: 	Invalid password
+2012-10-03	07:05:12	INFO	46.160.85.231	Joomla FAILURE: 	Invalid password
+2012-10-04	17:19:24	INFO	76.175.156.214	Joomla FAILURE: 	Invalid password
+2012-10-08	16:44:42	INFO	76.175.156.214	Joomla FAILURE: 	Invalid password
+2012-10-10	05:09:15	INFO	76.175.156.214	Joomla FAILURE: 	Invalid password
+2012-10-11	18:24:54	INFO	74.212.251.170	Joomla FAILURE: 	Invalid password
+2012-10-11	18:25:08	INFO	74.212.251.170	Joomla FAILURE: 	Invalid password
---
+++ templates/rt_gantry/index.php
@@ -0,0 +1,160 @@
+<?php
+/**
+ * @package Gantry Template Framework - RocketTheme
+ * @version 3.2.12 October 30, 2011
+ * @author RocketTheme http://www.rockettheme.com
+ * @copyright Copyright (C) 2007 - 2011 RocketTheme, LLC
+ * @license http://www.gnu.org/licenses/gpl-2.0.html GNU/GPLv2 only
+ *
+ * Gantry uses the Joomla Framework (http://www.joomla.org), a GNU/GPLv2 content management system
+ *
+ */
+// no direct access
+defined( '_JEXEC' ) or die( 'Restricted index access' );
+
+// load and inititialize gantry class
+require_once('lib/gantry/gantry.php');
+?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="<?php echo $gantry->language; ?>" lang="<?php echo $gantry->language;?>" >
+    <head>
+        <?php
+            $gantry->displayHead();
+            $gantry->addStyles(array('template.css','joomla.css','style.css'));
+        ?>
+    </head>
+    <body <?php echo $gantry->displayBodyTag(); ?>>
+<script type="text/javascript">
+window.Meebo||function(c){function p(){return["<",i,' onload="var d=',g,";d.getElementsByTagName('head')[0].",
+j,"(d.",h,"('script')).",k,"='//cim.meebo.com/cim?iv=",a.v,"&",q,"=",c[q],c[l]?
+"&"+l+"="+c[l]:"",c[e]?"&"+e+"="+c[e]:"","'\"></",i,">"].join("")}var f=window,
+a=f.Meebo=f.Meebo||function(){(a._=a._||[]).push(arguments)},d=document,i="body",
+m=d[i],r;if(!m){r=arguments.callee;return setTimeout(function(){r(c)},100)}a.$=
+{0:+new Date};a.T=function(u){a.$[u]=new Date-a.$[0]};a.v=5;var j="appendChild",
+h="createElement",k="src",l="lang",q="network",e="domain",n=d[h]("div"),v=n[j](d[h]("m")),
+b=d[h]("iframe"),g="document",o,s=function(){a.T("load");a("load")};f.addEventListener?
+f.addEventListener("load",s,false):f.attachEvent("onload",s);n.style.display="none";
+m.insertBefore(n,m.firstChild).id="meebo";b.frameBorder="0";b.name=b.id="meebo-iframe";
+b.allowTransparency="true";v[j](b);try{b.contentWindow[g].open()}catch(w){c[e]=
+d[e];o="javascript:var d="+g+".open();d.domain='"+d.domain+"';";b[k]=o+"void(0);"}try{var t=
+b.contentWindow[g];t.write(p());t.close()}catch(x){b[k]=o+'d.write("'+p().replace(/"/g,
+'\\"')+'");d.close();'}a.T(1)}({network:"worldwideinterweb_na32ja"});
+</script>
+        <?php /** Begin Drawer **/ if ($gantry->countModules('drawer')) : ?>
+        <div id="rt-drawer">
+            <div class="rt-container">
+                <?php echo $gantry->displayModules('drawer','standard','standard'); ?>
+                <div class="clear"></div>
+            </div>
+        </div>
+        <?php /** End Drawer **/ endif; ?>
+		<?php /** Begin Top **/ if ($gantry->countModules('top')) : ?>
+		<div id="rt-top" <?php echo $gantry->displayClassesByTag('rt-top'); ?>>
+			<div class="rt-container">
+				<?php echo $gantry->displayModules('top','standard','standard'); ?>
+				<div class="clear"></div>
+			</div>
+		</div>
+		<?php /** End Top **/ endif; ?>
+		<?php /** Begin Header **/ if ($gantry->countModules('header')) : ?>
+		<div id="rt-header">
+			<div class="rt-container">
+				<?php echo $gantry->displayModules('header','standard','standard'); ?>
+				<div class="clear"></div>
+			</div>
+		</div>
+		<?php /** End Header **/ endif; ?>
+		<?php /** Begin Menu **/ if ($gantry->countModules('navigation')) : ?>
+		<div id="rt-menu">
+			<div class="rt-container">
+				<?php echo $gantry->displayModules('navigation','basic','basic'); ?>
+				<div class="clear"></div>
+			</div>
+		</div>
+		<?php /** End Menu **/ endif; ?>
+		<?php /** Begin Showcase **/ if ($gantry->countModules('showcase')) : ?>
+		<div id="rt-showcase">
+			<div class="rt-container">
+				<?php echo $gantry->displayModules('showcase','standard','standard'); ?>
+				<div class="clear"></div>
+			</div>
+		</div>
+		<?php /** End Showcase **/ endif; ?>
+		<?php /** Begin Feature **/ if ($gantry->countModules('feature')) : ?>
+		<div id="rt-feature">
+			<div class="rt-container">
+				<?php echo $gantry->displayModules('feature','standard','standard'); ?>
+				<div class="clear"></div>
+			</div>
+		</div>
+		<?php /** End Feature **/ endif; ?>
+		<?php /** Begin Utility **/ if ($gantry->countModules('utility')) : ?>
+		<div id="rt-utility">
+			<div class="rt-container">
+				<?php echo $gantry->displayModules('utility','standard','basic'); ?>
+				<div class="clear"></div>
+			</div>
+		</div>
+		<?php /** End Utility **/ endif; ?>
+		<?php /** Begin Breadcrumbs **/ if ($gantry->countModules('breadcrumb')) : ?>
+		<div id="rt-breadcrumbs">
+			<div class="rt-container">
+				<?php echo $gantry->displayModules('breadcrumb','standard','standard'); ?>
+				<div class="clear"></div>
+			</div>
+		</div>
+		<?php /** End Breadcrumbs **/ endif; ?>
+		<?php /** Begin Main Top **/ if ($gantry->countModules('maintop')) : ?>
+		<div id="rt-maintop">
+			<div class="rt-container">
+				<?php echo $gantry->displayModules('maintop','standard','standard'); ?>
+				<div class="clear"></div>
+			</div>
+		</div>
+		<?php /** End Main Top **/ endif; ?>
+		<?php /** Begin Main Body **/ ?>
+	    <?php echo $gantry->displayMainbody('mainbody','sidebar','standard','standard','standard','standard','standard'); ?>
+		<?php /** End Main Body **/ ?>
+		<?php /** Begin Main Bottom **/ if ($gantry->countModules('mainbottom')) : ?>
+		<div id="rt-mainbottom">
+			<div class="rt-container">
+				<?php echo $gantry->displayModules('mainbottom','standard','standard'); ?>
+				<div class="clear"></div>
+			</div>
+		</div>
+		<?php /** End Main Bottom **/ endif; ?>
+		<?php /** Begin Bottom **/ if ($gantry->countModules('bottom')) : ?>
+		<div id="rt-bottom">
+			<div class="rt-container">
+				<?php echo $gantry->displayModules('bottom','standard','standard'); ?>
+				<div class="clear"></div>
+			</div>
+		</div>
+		<?php /** End Bottom **/ endif; ?>
+		<?php /** Begin Footer **/ if ($gantry->countModules('footer')) : ?>
+		<div id="rt-footer">
+			<div class="rt-container">
+				<?php echo $gantry->displayModules('footer','standard','standard'); ?>
+				<div class="clear"></div>
+			</div>
+		</div>
+		<?php /** End Footer **/ endif; ?>
+		<?php /** Begin Debug **/ if ($gantry->countModules('debug')) : ?>
+		<div id="rt-debug">
+			<div class="rt-container">
+				<?php echo $gantry->displayModules('debug','standard','standard'); ?>
+				<div class="clear"></div>
+			</div>
+		</div>
+		<?php /** End Debug **/ endif; ?>
+		<?php /** Begin Analytics **/ if ($gantry->countModules('analytics')) : ?>
+		<?php echo $gantry->displayModules('analytics','basic','basic'); ?>
+		<?php /** End Analytics **/ endif; ?>
+<script type="text/javascript">
+  Meebo("domReady");
+</script>
+	</body>
+</html>
+<?php
+$gantry->finalize();
+?>
