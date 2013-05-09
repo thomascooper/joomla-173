99b594356c82fea896568495e6caf20216b24fcb
root <root@wwi-www2.worldwideinterweb.com>
2013-05-08 20:01:42 -0400

    added jbetolo

---
+++ components/com_jbetolo/jbetolo.php
@@ -0,0 +1,14 @@
+<?php
+/**
+* @version:	2.5.0.44e92e4 - 2013 April 08 08:34:00 +0300
+* @package:	jbetolo
+* @subpackage:	jbetolo
+* @copyright:	Copyright (C) 2010 - 2013 jproven.com. All rights reserved. 
+* @license:	GNU General Public License Version 2, or later http://www.gnu.org/licenses/gpl.html
+*/
+ 
+defined('_JEXEC') or die('Restricted access');
+
+require_once JPATH_ADMINISTRATOR.'/components/com_jbetolo/jbetolo.php';
+
+?>
