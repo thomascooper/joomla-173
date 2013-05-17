33a25f9b8200f7be7dd061ee1737cb5cb78d0b5d
root <root@wwi-nginx06.worldwideinterweb.com>
2013-05-16 23:13:57 -0400

    started adding css min

--- plugins/system/fullpagememcache/fullpagememcache.php
+++ plugins/system/fullpagememcache/fullpagememcache.php
@@ -13,11 +13,17 @@ class  plgSystemFullPageMemcache extends JPlugin
 	function onAfterRender() {
 		if(class_exists('Memcached')) {
 			$mainframe = JFactory::getApplication();
-			$req_token = str_replace(' ','%20',$_SERVER['REQUEST_URI'].$_SERVER['HTTP_USER_AGENT']);
-			$utf_token = mb_convert_encoding($req_token, "UTF-8");
+        		//$body = JResponse::getBody();
+
+			// Get all local CSS files
+			//$matches['css'] = $this->getCssMatches($body);
+
+			// Remove all found files from body
+        		//$body = $this->cleanup($body, $matches);
+
 			$m = new Memcached();
 			$m->setOption(Memcached::OPT_BINARY_PROTOCOL, true);
-			$m->setOption(Memcached::OPT_COMPRESSION, true);
+			$m->setOption(Memcached::OPT_COMPRESSION, $mainframe->getCfg('gzip'));
 			$m->addServers(array(
 				array('10.1.1.36', 11211, 20)
 			));
@@ -26,7 +32,8 @@ class  plgSystemFullPageMemcache extends JPlugin
 			$key = $namespace.md5($_SERVER['REQUEST_METHOD'] . ' '. $_SERVER['HTTP_HOST'].$_SERVER['REQUEST_URI'].$_SERVER['HTTP_USER_AGENT']) . 0;
 
 			$buffer = JResponse::toString($mainframe->getCfg('gzip'));
-			$data = "EXTRACT_HEADERS\r\nContent-Type: gzip; charset=UTF-8\r\n";
+
+			$data = "EXTRACT_HEADERS\r\nContent-Type: text\html; charset=UTF-8\r\n";
 			$data .= "\r\n";
 			$data .= $buffer."\r\n";
 			$data .= '<!-- Memcached using key:'.$key.' @ '.date('Y-m-d H:i:s').' uri: '.$_SERVER['REQUEST_URI'].' //-->';
@@ -36,6 +43,113 @@ class  plgSystemFullPageMemcache extends JPlugin
 
 			$duration = $minutes * 60;
 			$ret = $m->add($key,$data,$duration);
+
+			echo $buffer;
 		}
 	}
+    /**
+     * Load the parameters
+     *
+     * @param null
+     * @return JParameter
+     */
+    private function getParams()
+    {
+        jimport('joomla.version');
+        $version = new JVersion();
+        if(version_compare($version->RELEASE, '1.5', 'eq')) {
+            $plugin = JPluginHelper::getPlugin('system', 'scriptmerge');
+            $params = new JParameter($plugin->params);
+            return $params;
+        } else {
+            return $this->params;
+        }
+    }
+
+    private function getCssMatches($body = null)
+    {
+        // Remove conditional comments from matching
+        $buffer = preg_replace('/<!--(.*)-->/msU', '', $body);
+
+        // Detect all CSS 
+        preg_match_all('/<link(.*)href="([^\"]+)"(.*)>/msU', $buffer, $matches);
+
+        // Parse all the matched entries
+        $files = array();
+        if(isset($matches[2])) {
+
+            // Get the exclude-matches
+            $exclude_css = explode(',', $this->getParams()->get('exclude_css'));
+            if(!empty($exclude_css)) {
+                foreach($exclude_css as $i => $e) {
+                    $e = trim($e);
+                    if(empty($e)) {
+                        unset($exclude_css[$i]);
+                    } else {
+                        $exclude_css[$i] = $e;
+                    }
+                }
+            }
+
+            // Loop through the rules
+            foreach($matches[2] as $index => $match) {
+
+                // Skip certain entries
+                if(strpos($matches[0][$index], 'stylesheet') == false && strpos($matches[0][$index], 'css') == false) continue;
+                if(strpos($matches[0][$index], 'media="print"')) continue;
+
+                // Only try to match local CSS
+                $match = str_replace(JURI::base(), '', $match);
+                $match = preg_replace('/^'.str_replace('/', '\/', JURI::base(true)).'/', '', $match);
+                if(preg_match('/\.css$/', $match) && !preg_match('/^http:\/\//', $match)) {
+
+                    // Only include files that can be read
+                    $file = $match;
+
+                    // Try to determine the path to this file
+                    $filepath = JPATH_BASE.$file;
+
+                    if(!empty($filepath)) {
+                        $files[] = array(
+                            'remote' => 0,
+                            'file' => $filepath,
+                            'html' => $matches[0][$index],
+                        );
+                    }
+                }
+            }
+        }
+
+        return $files;
+    }
+
+    /**
+     * Method to remove obsolete tags in the HTML body
+     *
+     * @param string $body 
+     * @param array $files
+     * @return string
+     */
+    private function cleanup($body = null, $matches = array())
+    {
+        foreach($matches as $typename => $type) {
+            if(!empty($type)) {
+
+                $first = true;
+                foreach($type as $file) {
+
+                    if($first) {
+                        $replacement = '<!-- plg_scriptmerge_'.md5($typename).' -->';
+                        $first = false;
+                    } else {
+                        $replacement = '';
+                    }
+
+                    $body = str_replace($file['html'], $replacement, $body);
+                }
+            }
+        }
+
+        return $body;
+    }
 }
