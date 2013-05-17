3b787628b4312d8158ae07411a0963aab316a349
root <root@wwi-nginx06.worldwideinterweb.com>
2013-05-16 23:46:39 -0400

    installed admin tools

---
+++ administrator/components/com_admintools/version.php
@@ -0,0 +1,7 @@
+<?php
+// Protect from unauthorized access
+defined('_JEXEC') or die('Restricted Access');
+
+define('ADMINTOOLS_VERSION', '2.5.5');
+define('ADMINTOOLS_DATE', '2013-05-11');
+define('ADMINTOOLS_PRO','0');
