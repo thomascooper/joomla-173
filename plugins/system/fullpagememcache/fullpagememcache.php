726434d069ce37f5480b3fe2bda2174b8eb38b72
root <root@nginx-test.smartlocalsocial.com>
2013-05-17 13:57:46 +1000

    whitespace fix no code changes

--- plugins/system/fullpagememcache/fullpagememcache.php
+++ plugins/system/fullpagememcache/fullpagememcache.php
@@ -14,39 +14,39 @@ class  plgSystemFullPageMemcache extends JPlugin
 		if(class_exists('Memcached')) {
 			$mainframe = JFactory::getApplication();
 			if(!$mainframe->isAdmin()) {
-        		//$body = JResponse::getBody();
+				//$body = JResponse::getBody();
 
-			// Get all local CSS files
-			//$matches['css'] = $this->getCssMatches($body);
+				// Get all local CSS files
+				//$matches['css'] = $this->getCssMatches($body);
 
-			// Remove all found files from body
-        		//$body = $this->cleanup($body, $matches);
+				// Remove all found files from body
+				//$body = $this->cleanup($body, $matches);
 
-			$m = new Memcached();
-			$m->setOption(Memcached::OPT_BINARY_PROTOCOL, true);
-			$m->setOption(Memcached::OPT_COMPRESSION, $mainframe->getCfg('gzip'));
-			$m->addServers(array(
-				array('10.1.1.36', 11211, 20)
-			));
+				$m = new Memcached();
+				$m->setOption(Memcached::OPT_BINARY_PROTOCOL, true);
+				$m->setOption(Memcached::OPT_COMPRESSION, $mainframe->getCfg('gzip'));
+				$m->addServers(array(
+					array('10.1.1.36', 11211, 20)
+				));
 
-			$namespace = '__ns__'.$_SERVER['HTTP_HOST'];
-			$key = $namespace.md5($_SERVER['REQUEST_METHOD'] . ' '. $_SERVER['HTTP_HOST'].$_SERVER['REQUEST_URI'].$_SERVER['HTTP_USER_AGENT']) . 0;
+				$namespace = '__ns__'.$_SERVER['HTTP_HOST'];
+				$key = $namespace.md5($_SERVER['REQUEST_METHOD'] . ' '. $_SERVER['HTTP_HOST'].$_SERVER['REQUEST_URI'].$_SERVER['HTTP_USER_AGENT']) . 0;
 
-			$buffer = JResponse::toString($mainframe->getCfg('gzip'));
+				$buffer = JResponse::toString($mainframe->getCfg('gzip'));
 
-			$data = "EXTRACT_HEADERS\r\nContent-Type: text\html; charset=UTF-8\r\n";
-			$data .= "\r\n";
-			$data .= $buffer."\r\n";
-			$data .= '<!-- Memcached using key:'.$key.' @ '.date('Y-m-d H:i:s').' uri: '.$_SERVER['REQUEST_URI'].' //-->';
+				$data = "EXTRACT_HEADERS\r\nContent-Type: text\html; charset=UTF-8\r\n";
+				$data .= "\r\n";
+				$data .= $buffer."\r\n";
+				$data .= '<!-- Memcached using key:'.$key.' @ '.date('Y-m-d H:i:s').' uri: '.$_SERVER['REQUEST_URI'].' //-->';
 
-			// Set duration in minutes
-			$minutes = 15;
+				// Set duration in minutes
+				$minutes = 15;
 
-			$duration = $minutes * 60;
-			$ret = $m->add($key,$data,$duration);
+				$duration = $minutes * 60;
+				$ret = $m->add($key,$data,$duration);
 
-			echo $buffer;
-		}
+				echo $buffer;
+			}
 		}
 	}
     /**
