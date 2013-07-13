aa26713041f32c60003794bf2d057be7942d1670
root <root@wwi-nginx06.worldwideinterweb.com>
2013-07-13 03:50:30 -0400

    added facebook like

--- templates/frontend/index.php
+++ templates/frontend/index.php
@@ -30,8 +30,17 @@ include_once JPATH_THEMES.'/'.$this->template.'/logic.php'; // load logic.php
 		<jdoc:include type="message" />
 		<jdoc:include type="component" />
 		<div class="clear"><!-- --></div>
-	</div>
-  <jdoc:include type="modules" name="simple-login" />
+  		<jdoc:include type="modules" name="simple-login" />
+<div id="fb-root"></div>
+<script>(function(d, s, id) {
+  var js, fjs = d.getElementsByTagName(s)[0];
+  if (d.getElementById(id)) return;
+  js = d.createElement(s); js.id = id;
+  js.src = "//connect.facebook.net/en_US/all.js#xfbml=1";
+  fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'facebook-jssdk'));</script>
+<div class="fb-like" data-href="http://www.facebook.com/worldwideinterweb" data-send="false" data-width="250" data-show-faces="false" data-colorscheme="light" data-font="tahoma" style="color:white"></div>
+</div>
   <jdoc:include type="modules" name="debug" />
 </body>
 
