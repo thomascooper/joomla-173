49a6acae790f0d81372fc66ba1fb3df7c556a58b
root <root@wwi-nginx06.worldwideinterweb.com>
2013-08-09 13:58:28 -0400

    added ga to contest page

--- templates/frontend/index.php
+++ templates/frontend/index.php
@@ -26,6 +26,19 @@ include_once JPATH_THEMES.'/'.$this->template.'/logic.php'; // load logic.php
       </style>
     <?php endif; ?>
   <![endif]-->
+<script type="text/javascript">
+
+  var _gaq = _gaq || [];
+  _gaq.push(['_setAccount', 'UA-27023314-1']);
+  _gaq.push(['_trackPageview']);
+
+  (function() {
+    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+  })();
+
+</script>
 </head>
   
 <body class="<?php echo (($menu->getActive() == $menu->getDefault()) ? ('front') : ('page')).' '.$active->alias.' '.$pageclass; ?>">
