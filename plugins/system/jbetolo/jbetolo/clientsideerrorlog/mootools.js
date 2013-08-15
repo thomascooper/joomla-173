1f7a633743b5214f0186bc394ea6b6030ecdcc57
root <root@wwi-nginx06.worldwideinterweb.com>
2013-08-15 00:49:30 -0400

    removed jbetolo, lazyload and scriptmerge

--- plugins/system/jbetolo/jbetolo/clientsideerrorlog/mootools.js
+++
@@ -1,9 +0,0 @@
-function jbetoloLogError(message, file, line) {
-        new Request.JSON({
-                url: document.location.href+'/index.php?option=com_jbetolo&task=logclientsideerror',
-                method: 'post',
-                data: {json:{context:navigator.userAgent, message:message, file:file, line:line}}
-        }).send();
-}
-
-window.onerror = function(message, file, line) { jbetoloLogError(message, file, line); };
